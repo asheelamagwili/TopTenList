7f57b9adeaaf7d271ba3d48c246efa4f61f0a0b8
Asheela Magwili <asheelamagwili@wlan-t-201-211-dhcp.CSUChico.EDU>
2020-04-01 14:01:02 -0700

    Interface Implementation

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -7,7 +7,7 @@ using std::string;
 
 TopTenList::TopTenList()
 {
-    _list.resize(10);
+    _list.resize(11);
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
@@ -18,4 +18,36 @@ void TopTenList::set_at(int index, Hyperlink link)
 Hyperlink TopTenList::get(int index)
 {
     return _list.at(index);
+}
+
+void TopTenList::display_forward()
+{
+    if( _list.size() == 0 )
+    {
+        std::cout<<" is empty.\n";
+    }
+
+    else
+    {
+        for(int i = 1; i <= (int)_list.size()-1; i++)
+        {
+          std::cout<<i<<". "<<_list[i].url<<std::endl;
+        }
+    }
+}
+
+void TopTenList::display_backward()
+{
+    if( _list.size() == 0 )
+    {
+        std::cout<<" is empty.\n";
+    }
+
+    else
+    {
+        for(int i = (int)_list.size()-1; i >= 0; i--)
+        {
+          std::cout<<i<<". "<<_list[i].url<<std::endl;
+        }
+    }
 }
